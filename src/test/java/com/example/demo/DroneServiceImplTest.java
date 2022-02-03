766bc814a999e1e647281db57bac73b086c255fc
rghdslh3 <rghdslh3@gmail.com>
2022-02-03 11:07:29 +0200

    Update DroneServiceImplTest.java

--- src/test/java/com/example/demo/DroneServiceImplTest.java
+++ src/test/java/com/example/demo/DroneServiceImplTest.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import static org.mockito.Mockito.when;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,7 +15,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.example.demo.enums.DroneStateEnum;
 import com.example.demo.io.entity.DroneEntity;
+import com.example.demo.io.entity.MedicationEntity;
 import com.example.demo.service.impl.DroneServiceImpl;
 import com.example.demo.shared.Utils;
 import com.example.demo.shared.dto.DroneDto;
@@ -25,6 +29,9 @@ class DroneServiceImplTest {
 	
 	@Mock
 	Utils util;
+	
+	@Mock
+	MedicationRepository medicationRepository;
 	
 	@InjectMocks
 	DroneServiceImpl droneService;
@@ -82,5 +89,62 @@ class DroneServiceImplTest {
 		Assertions.assertEquals(1,createdDroneDto.getModel());
 		Assertions.assertEquals(300,createdDroneDto.getWeight());
 		Assertions.assertEquals(80,createdDroneDto.getBattery());		
+	}
+	
+	@Test
+	void testCreateUser_RunTimeException() {
+		DroneDto droneDto = new DroneDto();
+		droneDto.setWeight(600);
+		droneDto.setBattery(110);
+		Assertions.assertThrows(RuntimeException.class,() -> droneService.createDrone(droneDto));
+	}
+	
+	@Test
+	void testUpdateDrone() {
+		DroneDto droneDto = new DroneDto();
+		droneDto.setState(DroneStateEnum.IDLE);
+		droneDto.setBattery(70);
+		when(droneRepository.findBySerial(ArgumentMatchers.anyString())).thenReturn(droneEntity);
+		droneEntity.setState(DroneStateEnum.IDLE);
+		droneEntity.setBattery(70);
+		when(droneRepository.save(ArgumentMatchers.any(DroneEntity.class))).thenReturn(droneEntity);
+		DroneDto updatedDroneDto = droneService.updateDrone(serial,droneDto);
+		Assertions.assertNotNull(updatedDroneDto);
+		Assertions.assertEquals(DroneStateEnum.IDLE,updatedDroneDto.getState());
+		Assertions.assertEquals(70,updatedDroneDto.getBattery());
+		}
+	
+	@Test
+	void testUpdateDrone_RunTimeException() {
+		DroneDto droneDto = new DroneDto();
+		droneDto.setBattery(110);
+		droneDto.setState(DroneStateEnum.LOADED);
+		Assertions.assertThrows(RuntimeException.class,() -> droneService.updateDrone(serial,droneDto));
+	}
+	
+	@Test
+	void testLoadMedicationItems() {
+		when(droneRepository.findBySerial(ArgumentMatchers.anyString())).thenReturn(droneEntity);
+		when(droneRepository.save(ArgumentMatchers.any(DroneEntity.class))).thenReturn(droneEntity);
+		List<String> medicationCodes = new ArrayList<String>();
+		medicationCodes.add("435ergr");
+		MedicationEntity medicationEntity = new MedicationEntity();
+		medicationEntity.setCode("435ergr");
+		medicationEntity.setWeight(300);
+		when(medicationRepository.findByCode(ArgumentMatchers.anyString())).thenReturn(medicationEntity);
+		DroneDto droneDto = droneService.loadMedicationItems(serial,medicationCodes);
+		Assertions.assertNotNull(droneDto);
+		Assertions.assertNotNull(droneDto.getMedications());
+		Assertions.assertEquals(DroneStateEnum.LOADED,droneDto.getState());
+	}
+	
+	@Test
+	void testLoadMedicationItems_RunTimeException() {
+		when(droneRepository.findBySerial(ArgumentMatchers.anyString())).thenReturn(null);
+		DroneDto droneDto = new DroneDto();
+		droneDto.setBattery(23);
+		List<String> medicationCodes = new ArrayList<String>();
+		medicationCodes.add("435ergr");
+		Assertions.assertThrows(RuntimeException.class,() -> droneService.loadMedicationItems(serial,medicationCodes));
 	}
 }
